8f98a49af0ad6604d39760f1f3d1082602c06a7a
Raoul1996 <baobaozhou199606@gmail.com>
2017-08-27 17:03:16 +0800

    修改index.js 分别渲染title和content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const appState = {
+  title: {
+    text: 'React little book',
+    color: 'red'
+  },
+  content: {
+    text: 'the content of react little book',
+    color: 'blue'
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function renderApp(appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle(title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+
+}
+renderApp(appState)

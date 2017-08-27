ce8fd6be169ade0bde1df7a46975ddac4987fb0e
Raoul1996 <baobaozhou199606@gmail.com>
2017-08-27 17:19:12 +0800

    抽离 store

--- src/index.js
+++ src/index.js
@@ -9,19 +9,26 @@ const appState = {
   }
 }
 
+function createStore(state, stateChanger) {
+  const getState = () => state
+  const dispatch = (action) => stateChanger(state, action)
+  return {getState, dispatch}
+}
 
-function dispatch(action) {
+function stateChanger(state, action) {
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      appState.title.text = action.text
+      state.title.text = action.text
       break
     case 'UPDATE_TITLE_COLOR':
-      appState.title.color = action.color
+      state.title.color = action.color
       break
     default:
       break
   }
 }
+
+const store = createStore(appState, stateChanger)
 
 function renderApp(appState) {
   renderTitle(appState.title)
@@ -41,8 +48,8 @@ function renderContent(content) {
 
 }
 
-renderApp(appState)
+renderApp(store.getState())
 
-dispatch({type: 'UPDATE_TITLE_TEXT', text: 'Raoul\'s lover already leave'})
-dispatch({type: 'UPDATE_TITLE_COLOR', color: 'gray'})
-renderApp(appState)
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'Raoul\'s lover already leave'})
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'gray'})
+renderApp(store.getState())
